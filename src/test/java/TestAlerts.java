0ad15ab268fe7b4e39434d5d5a9fb07534c21192
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-08-09 02:53:26 +0300

    Update TestAlerts.java

--- src/test/java/TestAlerts.java
+++ src/test/java/TestAlerts.java
@@ -1,12 +1,8 @@
 import actions.ElementActions;
 import driver_manager.DriverInitializer;
-import driver_manager.DriverManager;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.touch.TouchActions;
 import org.testng.annotations.Test;
-
-import javax.swing.*;
 
 public class TestAlerts extends DriverInitializer {
     private static final By ALERT_VIEWS_BUTTON = AppiumBy.accessibilityId("Alert Views");
