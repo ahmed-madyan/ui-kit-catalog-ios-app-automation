73bfd103bc160f60b19909f4a30c759850ca2aa5
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-08-11 03:15:44 +0300

    Update TestIOSPicker.java

--- src/test/java/TestIOSPicker.java
+++ src/test/java/TestIOSPicker.java
@@ -1,12 +1,10 @@
-import assertions.Assertions;
 import driver.DriverInitializer;
-import elements.Elements;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class TestIOSPicker extends DriverInitializer {
-    private static final By ALERT_VIEWS_BUTTON = AppiumBy.accessibilityId("Alert Views");
+    private static final By PICKER_VIEW_BUTTON = AppiumBy.accessibilityId("Picker View");
     private static final By TEXT_ENTRY_BUTTON = AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`name=='Text Entry'`]");
     private static final By TEXT_ENTRY_ALERT_TEXT_BOX = AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeTextField'");
     private static final By TEXT_ENTRY_ALERT_OK_BUTTON = AppiumBy.accessibilityId("OK");
@@ -17,26 +15,7 @@ public class TestIOSPicker extends DriverInitializer {
     private static final By CONFIRM_CANCEL_ALERT_CANCEL_BUTTON = AppiumBy.accessibilityId("Confirm");
 
     @Test
-    public void testIOSAlerts() {
-        Elements
-                .elementActions()
-                .click(ALERT_VIEWS_BUTTON)
-                .click(TEXT_ENTRY_BUTTON)
-                .click(TEXT_ENTRY_ALERT_Cancel_BUTTON)
-                .click(TEXT_ENTRY_BUTTON)
-                .sendKeys(TEXT_ENTRY_ALERT_TEXT_BOX, "Ahmed")
-                .click(TEXT_ENTRY_ALERT_OK_BUTTON);
+    public void testIOSPicker() {
 
-        Elements.elementActions()
-                .click(CONFIRM_CANCEL_ALERT_BUTTON);
-        Assertions.hardAssert()
-                .elementDisplayed(CONFIRM_CANCEL_ALERT_MESSAGE_TEXT)
-                .elementTextToBe(CONFIRM_CANCEL_ALERT_MESSAGE_TEXT, "A message should be a short, complete sentence.");
-        Elements.elementActions()
-                .click(CONFIRM_CANCEL_ALERT_CANCEL_BUTTON);
-        Assertions.hardAssert().elementNotDisplayed(CONFIRM_CANCEL_ALERT_MESSAGE_TEXT);
-        Elements.elementActions()
-                .click(CONFIRM_CANCEL_ALERT_BUTTON)
-                .click(CONFIRM_CANCEL_ALERT_CONFIRM_BUTTON);
     }
 }
