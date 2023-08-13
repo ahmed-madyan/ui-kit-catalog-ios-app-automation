38a0308a2861dd1eeba2a8bbf837a1f3979cea63
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-08-13 17:40:44 +0300

    Create TestIOSSliding.java

---
+++ src/test/java/TestIOSSliding.java
@@ -0,0 +1,20 @@
+import driver.DriverInitializer;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class TestIOSSliding extends DriverInitializer {
+    private static final By ALERT_VIEWS_BUTTON = AppiumBy.accessibilityId("Alert Views");
+    private static final By TEXT_ENTRY_BUTTON = AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`name=='Text Entry'`]");
+    private static final By TEXT_ENTRY_ALERT_TEXT_BOX = AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeTextField'");
+    private static final By TEXT_ENTRY_ALERT_OK_BUTTON = AppiumBy.accessibilityId("OK");
+    private static final By TEXT_ENTRY_ALERT_Cancel_BUTTON = AppiumBy.accessibilityId("Cancel");
+    private static final By CONFIRM_CANCEL_ALERT_BUTTON = AppiumBy.accessibilityId("Confirm / Cancel");
+    private static final By CONFIRM_CANCEL_ALERT_MESSAGE_TEXT = AppiumBy.iOSNsPredicateString("name BEGINSWITH[c] 'A message'");
+    private static final By CONFIRM_CANCEL_ALERT_CONFIRM_BUTTON = AppiumBy.accessibilityId("Cancel");
+    private static final By CONFIRM_CANCEL_ALERT_CANCEL_BUTTON = AppiumBy.accessibilityId("Confirm");
+
+    @Test
+    public void testIOSSliding() {
+    }
+}
